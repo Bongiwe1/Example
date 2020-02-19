54a0dad395f83158ea1dead0362b1a4a8cd35a8a
Bongiwe1 <61003365+Bongiwe1@users.noreply.github.com>
2020-02-19 10:49:02 +0200

    Add files via upload

---
+++ SPRINTS/stock_data2.sql
@@ -0,0 +1,46 @@
+"Stock Item",Size,"Product type",Quantity,"Stock Unit Price","Stock price","Selling Unit Price","Stock Selling Price","Expected Profit","Shelf number"
+Simba,"36 g",Chips,50,5,250,6,300,50,1
+Lays,"36 g",Chips,50,6,300,7,350,50,1
+Doritos,"36 g",Chips,50,8,400,9,450,50,1
+Fritos,"36 g",Chips,50,2,100,3,150,50,1
+"Popcorns ","36 g",Chips,50,4,200,5,250,50,8
+Coke,"340 ml",Cooldrink,25,10,250,11,275,25,2
+Fanta,"340 ml",Cooldrink,25,10,250,11,275,25,2
+Sprite,"340 ml",Cooldrink,25,10,250,11,275,25,2
+Stoney,"340 ml",Cooldrink,25,10,250,11,275,25,2
+Pepsi,"340 ml",Cooldrink,25,10,250,11,275,25,2
+Cadbury,"48 g",Chocolate,30,8,240,9,270,30,3
+Tex,"48 g",Chocolate,30,5,150,6,180,30,3
+"Lunch Bar","48 g",Chocolate,30,5,150,6,180,30,3
+Crisp,"48 g",Chocolate,40,5,200,6,240,40,3
+P.S,"48 g",Chocolate,40,6,240,7,280,40,3
+"Pepper Steak","160 g",Pie,25,15,375,18,450,25,4
+Chicken,"160 g",Pie,25,12,300,15,375,75,4
+"Egg and Ham ","160 g",Pie,25,12,300,15,375,50,8
+Vegetable,"160 g",Pie,20,15,300,18,360,60,4
+Mince,"160 g",Pie,25,12,300,15,375,75,4
+Vanilla,single,Cupcake,25,6,150,9,225,75,4
+Bran,single,Cupcake,25,6,150,9,225,75,4
+Caramel,single,Cupcake,25,6,150,9,225,75,4
+Blueberry,single,Cupcake,25,7,175,10,250,75,4
+"Thin mint",single,Cupcake,25,7,175,10,250,75,4
+"Spinach ","1 pack",Veggies,20,7,140,10,200,50,8
+Cabbage,"1 piece",Veggies,20,7,140,10,200,60,5
+Potato,5kg,Veggies,30,20,600,25,750,50,5
+Squash,1kg,Veggies,20,12,240,15,300,60,5
+Carrots,1kg,Veggies,30,7,210,10,300,90,5
+Apple,1kg,Fruits,30,7,210,12,360,50,6
+Pear,1kg,Fruits,30,7,210,12,360,50,6
+Orange,1kg,Fruits,30,8,240,15,450,210,6
+Peach,1kg,Fruits,30,9,270,16,480,160,6
+Kiwi,1kg,Fruits,20,10,200,18,360,125,6
+Magnum,"250 ml","Ice lolly",25,12,300,17,425,50,7
+"Mini Milk","250 ml","Ice lolly",25,10,250,12,300,50,7
+Calippo,"250 ml","Ice lolly",25,11,275,13,325,50,7
+Solero,"250 ml","Ice lolly",25,10,250,12,300,50,7
+Zoom,"250 ml","Ice lolly",25,8,200,10,250,50,7
+"Black pepper","7 g",Spices,50,2,100,3,150,50,8
+"Spicy barbecue","7 g",Spices,50,2,100,3,150,50,8
+"Rajah medium","7 g",Spices,50,2,100,3,150,50,8
+Peri-Peri,"7 g",Spices,50,2,100,3,150,50,8
+Benny,"7 g",Spices,50,2,100,3,150,50,8
